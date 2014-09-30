afddba17835f16e2d95a05d71b6b39978ecf96fe
Michael Specht <micha.specht@gmail.com>
2014-09-30 21:20:43 +0200

    updated release script

--- build.rb
+++ build.rb
@@ -10,14 +10,12 @@ system("git archive #{tag} | gzip -c > _build/src/ag_#{tag}.tar.gz")
 system("cd _build/src && tar xvzf ag_#{tag}.tar.gz")
 
 File::open('_build/src/debian/changelog', 'w') do |f|
-    ['lucid', 'precise', 'trusty', 'utopic'].each do |release|
-        f.puts "ag (#{tag}) #{release}; urgency=low"
-        f.puts
-        f.puts "  * Initial release"
-        f.puts
-        f.puts " -- Michael Specht <micha.specht@gmail.com>  Thu,  31 Jul 2014 16:00:00 +0200"
-        f.puts
-    end
+    f.puts "ag (#{tag}) precise; urgency=low"
+    f.puts
+    f.puts "  * Initial release"
+    f.puts
+    f.puts " -- Michael Specht <micha.specht@gmail.com>  Thu,  31 Jul 2014 16:00:00 +0200"
+    f.puts
 end
 
 system("cd _build/src && debuild -S")
--- publish.rb
+++ publish.rb
@@ -3,4 +3,22 @@
 tag = `git tag | sort | tail -n 1`.strip
 puts "Publishing Ag #{tag}..."
 
-system("dput ppa:micha-specht/ag _build/ag_#{tag}_source.changes")
+['lucid', 'precise', 'trusty', 'utopic'].each do |release|
+    system("rm -rf _build/src")
+    system("rm -rf _build/ag_#{tag}*")
+    system("mkdir -p _build/src")
+    system("git archive #{tag} | gzip -c > _build/src/ag_#{tag}.tar.gz")
+    system("cd _build/src && tar xvzf ag_#{tag}.tar.gz")
+
+    File::open('_build/src/debian/changelog', 'w') do |f|
+        f.puts "ag (#{tag}) #{release}; urgency=low"
+        f.puts
+        f.puts "  * Initial release"
+        f.puts
+        f.puts " -- Michael Specht <micha.specht@gmail.com>  Thu,  31 Jul 2014 16:00:00 +0200"
+        f.puts
+    end
+
+    system("cd _build/src && debuild -S")
+    system("dput ppa:micha-specht/ag _build/ag_#{tag}_source.changes")
+end
