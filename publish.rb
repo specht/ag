8ef20a3d8b939e552ea54c9e07a1daed9023cecc
Michael Specht <micha.specht@gmail.com>
2014-12-05 09:28:14 +0100

    [sx6569] Fix date in manpage

--- publish.rb
+++ publish.rb
@@ -13,12 +13,18 @@ puts "Publishing Ag #{tag}..."
     File::open('_build/src/debian/changelog', 'w') do |f|
         f.puts "ag (#{tag}~#{release}) #{release}; urgency=low"
         f.puts
-        f.puts "  * Initial release"
+        f.puts "  * Update"
         f.puts
-        f.puts " -- Michael Specht <micha.specht@gmail.com>  Thu,  31 Jul 2014 16:00:00 +0200"
+        f.puts " -- Michael Specht <micha.specht@gmail.com>  #{Time.now.strftime('%a,  %d %b %Y %T %z')}"
         f.puts
     end
 
+    manpage = File::read('_build/src/debian/ag.1')
+    manpage.gsub!('#{DATE}', Time.now.strftime('%B %d, %Y'))
+    File::open('_build/src/debian/ag.1', 'w') do |f|
+        f.write(manpage)
+    end
+    
     system("cd _build/src && debuild -S")
     system("dput ppa:micha-specht/ag _build/ag_#{tag}~#{release}_source.changes")
 end
